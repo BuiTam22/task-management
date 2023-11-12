cd7faa8c8f2dcb2c0878efb97acb756114ef1972
Tam <Tam@TAM-DESKTOP>
2023-11-12 09:29:35 +0700

    tạo controller và đưa model vào /api/v1

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -1,29 +1,10 @@
 const express = require("express");
 const router  = express.Router();
+const controller = require("../../../api/v1/controllers/task.controller.js");
+const Task = require("../models/task.model.js");
 
-const Task = require("../../../models/task.model");
+router.get("/", controller.index)
 
-router.get("/", async (req, res) => {
-  const tasks = await Task.find({
-    deleted: false
-  });
-
-  res.json(tasks);
-});
-
-router.get("/detail/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-
-    const task = await Task.findOne({
-      _id: id,
-      deleted: false
-    });
-
-    res.json(task);
-  } catch (error) {
-    res.json("Không tìm thấy!");
-  }
-});
+router.get("/detail/:id", controller.detail);
 
 module.exports = router;
