2062a6b1875ba309b0ffa3fb149f59455688313c
Tam <Tam@TAM-DESKTOP>
2023-11-14 19:06:13 +0700

    thêm thư viện CORS

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router  = express.Router();
 const controller = require("../../../api/v1/controllers/task.controller.js");
-const Task = require("../models/task.model.js");
 
 router.get("/", controller.index)
 
